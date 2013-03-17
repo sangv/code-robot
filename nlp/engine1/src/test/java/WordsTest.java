94043cd945f00e9d66acee746e55f694f91fa427
sang <sang.venkatraman@gmail.com>
2013-03-17 13:13:50 -0400

    replicating results

--- nlp/engine1/src/test/java/WordsTest.java
+++ nlp/engine1/src/test/java/WordsTest.java
@@ -12,28 +12,40 @@ public class WordsTest {
 
     static Map<WordTag,Integer> wordTagCountMap = new HashMap<WordTag, Integer>();
 
+    static List<String> wordTagResults = new ArrayList<String>();
+
     static Map<NGramTag,Integer> oneGramCountMap = new HashMap<NGramTag, Integer>();
 
     static List<String> oneGramResults = new ArrayList<String>();
 
-    static List<String> wordTagResults = new ArrayList<String>();
+    static Map<NGramTag,Integer> twoGramCountMap = new HashMap<NGramTag, Integer>();
+
+    static List<String> twoGramResults = new ArrayList<String>();
 
 
     public static void main(String[] args) throws Exception {
 
-        List<String> input = getContents("/home/sang/Source/code-robot/nlp/engine1/gene.train");
+        List<String> input = getContents("/home/sang/Source/code-robot/nlp/engine1/src/test/resources/gene.train");
 
         calculateWordTagCounts(input);
 
         calculateOneGramCounts(input);
 
+        printResults();
+
     }
 
     public static void calculateWordTagCounts(List<String> input){
+        List<WordTag> wordsInSentence = initWordsInSentence();
         for(String str: input){
-            if(str != null && str.length() > 0){
+            if(str == null || str.length() == 0) {
+                completeWordsInSentence(wordsInSentence);
+
+                wordsInSentence = initWordsInSentence();
+            } else {
                 String[] splits = str.split(" ");
                 WordTag wordTag = new WordTag(splits[0],splits[1]);
+                wordsInSentence.add(wordTag);
                 Integer count = 0;
                 if(wordTagCountMap.containsKey(wordTag)){
                     count =  wordTagCountMap.get(wordTag);
@@ -41,7 +53,7 @@ public class WordsTest {
                 wordTagCountMap.put(wordTag,++count);
             }
         }
-
+        completeWordsInSentence(wordsInSentence);//calling it once after for the last sentence
         assertCondition(wordTagCountMap.get(new WordTag("consensus","I-GENE")),new Integer("13"));
 
         Set<Map.Entry<WordTag,Integer>> entrySet = wordTagCountMap.entrySet();
@@ -71,6 +83,7 @@ public class WordsTest {
         }
 
         assertCondition(oneGramCountMap.get(new NGramTag(1,"I-GENE")),new Integer("41072"));
+        assertCondition(oneGramCountMap.get(new NGramTag(1,"O")),new Integer("345128"));
 
         Set<Map.Entry<NGramTag,Integer>> entrySet = oneGramCountMap.entrySet();
         Iterator<Map.Entry<NGramTag,Integer>> iter = entrySet.iterator();
@@ -83,6 +96,47 @@ public class WordsTest {
             System.out.println(oneGramResult);
         }
 
+    }
+
+    public static void calculateTwoGramCounts(List<WordTag> sentence){
+        WordTag[] wordTagsInSentence = sentence.toArray(new WordTag[]{});
+
+        for(int i=0; i< wordTagsInSentence.length; ++i){
+                String tagBefore = wordTagsInSentence[i].getTag();
+                if(i < wordTagsInSentence.length - 1){
+                    String tagAfter = wordTagsInSentence[i+1].getTag();
+                    NGramTag nGramTag = new NGramTag(2,tagBefore,tagAfter);
+                    int count = 0;
+                    if(twoGramCountMap.containsKey(nGramTag)){
+                        count =  twoGramCountMap.get(nGramTag);
+                    }
+                    twoGramCountMap.put(nGramTag,++count);
+                }
+        }
+    }
+
+    public static void printResults(){
+
+        Set<Map.Entry<NGramTag,Integer>> entrySet = twoGramCountMap.entrySet();
+        Iterator<Map.Entry<NGramTag,Integer>> iter = entrySet.iterator();
+        while(iter.hasNext()){
+            Map.Entry<NGramTag,Integer> entry = iter.next();
+            twoGramResults.add(entry.getValue() + " 2-GRAM " + entry.getKey().getTag() + " " + entry.getKey().getOthers()[0]);
+        }
+
+        for(String twoGramResult: twoGramResults){
+            System.out.println(twoGramResult);
+        }
+
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"I-GENE","I-GENE")),new Integer("24435"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"O","I-GENE")),new Integer("15888"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"I-GENE","O")),new Integer("16624"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"O","STOP")),new Integer("13783"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"I-GENE","STOP")),new Integer("13"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"*","O")),new Integer("13047"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"O","O")),new Integer("315457"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"*","I-GENE")),new Integer("749"));
+        assertCondition(twoGramCountMap.get(new NGramTag(2,"*","*")),new Integer("13796"));
     }
 
     private static List<String> getContents(String fileLocation) throws IOException {
@@ -98,9 +152,21 @@ public class WordsTest {
 
 
     public static void assertCondition(Object actual, Object expected){
-        if(actual != null && expected != null && !expected.equals(actual)){
+        if(actual == null || expected == null || !expected.equals(actual)){
             throw new RuntimeException("Assertion failed. Got: " + actual + " Expected: " + expected);
         }
+    }
+
+    protected static List<WordTag> initWordsInSentence(){
+        List<WordTag> wordsInSentence = new ArrayList<WordTag>();
+        wordsInSentence.add(new WordTag("None","*"));
+        wordsInSentence.add(new WordTag("None","*"));
+        return wordsInSentence;
+    }
+
+    protected static void completeWordsInSentence(List<WordTag> wordsInSentence){
+        wordsInSentence.add(new WordTag("None","STOP"));//FIXME
+        calculateTwoGramCounts(wordsInSentence);
     }
 
 }
